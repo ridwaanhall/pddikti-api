4cf3a9e1b25fd0c06f188422d574e5dddf8a429f
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-20 15:26:30 +0000

    adds url hit_mhs

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.error_404),
+# ]
+
+# api/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_data),
+    path('', views.error_404),  # Example path for the error_404 view
+    path('hit_mhs/', views.hit_mhs_404),
+    path('hit_mhs/<str:hit_mhs>/', views.hit_mhs),  # Add a path with a dynamic parameter
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,48 @@
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework import status
+
+# # @api_view(['GET'])
+# # def test_data(request):
+# #     person = {'name':'Ridwan Halim', 'age':20}
+# #     return Response(person)
+# # https://api-frontend.kemdikbud.go.id/hit_mhs/ridwan%20halim%20uty
+
+# @api_view(['GET'])
+# def error_404(request):
+#     msg = {'message': '404 Not Found'}
+#     return Response(msg,status=status.HTTP_404_NOT_FOUND)
+
+# api/views.py
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import status
+import requests  # Import the requests module
 
 @api_view(['GET'])
-def test_data(request):
-    person = {'name':'Ridwan Halim', 'age':20}
-    return Response(person)
-# https://api-frontend.kemdikbud.go.id/hit_mhs/ridwan%20halim%20uty
+def error_404(request):
+    msg = {'message': '404 Not Found'}
+    return Response(msg, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def hit_mhs_404(request):
+    msg = {'message': '404 Not Found'}
+    return Response(msg, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def hit_mhs(request, hit_mhs):  # Added the hit_mhs parameter
+    # Construct the URL with the dynamic parameter
+    url = f'https://api-frontend.kemdikbud.go.id/hit_mhs/{hit_mhs}'
+
+    # Make a GET request to the specified URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the response content as JSON
+        data = response.json()
+        return Response(data, status=status.HTTP_200_OK)
+    else:
+        # If the request was not successful, return an message response
+        msg = {'message': 'Failed to fetch data'}
+        return Response(msg, status=response.status_code)
