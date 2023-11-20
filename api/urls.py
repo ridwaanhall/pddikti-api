ea5281b77a120f496bd5f64fd8e887a71c189fc4
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-20 17:05:29 +0000

    adds 404 for unavailable path

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,7 @@ from . import views
 
 urlpatterns = [
     path('', views.error_404),  # Example path for the error_404 view
+    path('<str:path>', views.error_404),
     path('hit_mhs/', views.hit_mhs_404),
     path('hit_mhs/<str:hit_mhs>/', views.hit_mhs),  # Add a path with a dynamic parameter
     path('detail_mhs/', views.detail_mhs_404),
